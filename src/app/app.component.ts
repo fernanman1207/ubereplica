ef4d08f0b81a3ba306104145136018d1278ae10b
unknown <SSDD@PNAWSA05LC1129.duoc.acad>
2024-08-21 09:30:08 -0400

    preparando todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
-// home.component.ts
-import { Component } from '@angular/core';
-import { MapService } from './map.service';
+import { Component, OnInit } from '@angular/core';
+import { MapService } from './map/map.service';
+import { GeolocationService } from './services/geolocation.service';
+import { RideRequestService } from './services/ride-request.service';
+import { UserService } from './services/user.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class HomeComponent {
-  constructor(private mapService: MapService) {}
+export class AppComponent implements OnInit {
+  title = 'UberClone';
+  user: any; // Assuming a user object with relevant properties
+
+  constructor(
+    private mapService: MapService,
+    private geolocationService: GeolocationService,
+    private rideRequestService: RideRequestService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.mapService.initMap();
+    this.geolocationService.getCurrentLocation().subscribe((location) => {
+      this.mapService.updateCurrentLocation(location);
+    });
+    this.userService.getUser().subscribe((user) => {
+      this.user = user;
+    });
   }
+
+  // Other methods for handling user interactions, ride requests, etc.
+  // ...
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapService } from './map.service';
+import { RideRequestService } from './services/ride-request.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomePage {
+export class HomeComponent implements OnInit {
+  constructor(
+    private mapService: MapService,
+    private rideRequestService: RideRequestService
+  ) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.mapService.initMap();
+  }
 
+  requestRide() {
+    // Lógica para solicitar un viaje
+    this.rideRequestService.requestRide().subscribe(
+      (response) => {
+        // Manejar la respuesta exitosa
+      },
+      (error) => {
+        // Manejar el error
+      }
+    );
+  }
 }
